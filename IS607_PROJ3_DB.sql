84191c993b523b1986a57ca05251818f5aeae296
LovinSpoonful <robhodde@yahoo.com>
2016-03-19 13:51:44 -0400

    ready for xforms - sample data - db on local machine

--- IS607_PROJ3_DB.sql
+++ IS607_PROJ3_DB.sql
@@ -8,16 +8,16 @@ USE skill;
 -- initial staging area for raw CSV's
 DROP TABLE IF EXISTS `tbl_import`;
 CREATE TABLE `tbl_import` (
-`source_id`           int(11) NOT NULL,
+`source_id`           int(11)   NOT NULL,
 `skill_type_name`     char(100) NOT NULL,
 `skill_name`          char(100) NOT NULL,
-`rating`              char(100) NOT NULL);
+`rating`              float     NOT NULL);
 
 -- distinct skills from tbl_import
 DROP TABLE IF EXISTS `tbl_skill`;
 CREATE TABLE `tbl_skill` (
-`skill_id`           int(11) NOT NULL auto_increment,
-`skill_type_id`      int(11) NOT NULL,
+`skill_id`           int(11)   NOT NULL auto_increment,
+`skill_type_id`      int(11)   NOT NULL,
 `skill_name`         char(100) NOT NULL,
 `skill_description`  char(255) NULL,
 PRIMARY KEY (`skill_id`));
@@ -25,7 +25,7 @@ PRIMARY KEY (`skill_id`));
 -- distinct skill types from tbl_import
 DROP TABLE IF EXISTS `tbl_skill_type`;
 CREATE TABLE `tbl_skill_type` (
-`skill_type_id`           int(11) NOT NULL,
+`skill_type_id`           int(11)   NOT NULL,
 `skill_type_name`         char(100) NOT NULL,
 `skill_type_description`  char(255) NULL,
 PRIMARY KEY (`skill_type_id`));
@@ -33,10 +33,10 @@ PRIMARY KEY (`skill_type_id`));
 -- distinct sources from tbl_import
 DROP TABLE IF EXISTS `tbl_source`;
 CREATE TABLE `tbl_source` (
-`source_id`           int(11) NOT NULL,
+`source_id`           int(11)   NOT NULL,
 `source_name`         char(100) NOT NULL,
 `source_description`  char(255) NULL,
-`source_URL`          char(255) NULL NULL,
+`source_URL`          char(255) NULL,
 PRIMARY KEY (`source_id`));
 
 ALTER TABLE tbl_skill
@@ -45,7 +45,37 @@ REFERENCES tbl_skill_type(skill_type_id)
 ON DELETE NO ACTION
 ON UPDATE CASCADE;
 
+-- normalized representation of imported data
+DROP TABLE IF EXISTS `tbl_data_n`;
+CREATE TABLE `tbl_data_n` (
+`skill_type_id`       int(11) NOT NULL,
+`skill_id`            int(11) NOT NULL,
+`source_id`           int(11) NOT NULL,
+`rating`              float   NOT NULL,
+PRIMARY KEY (`skill_type_id`, `skill_id`, `source_id`));
 
+ALTER TABLE tbl_data_n
+ADD FOREIGN KEY fk_skill_type(skill_type_id)
+REFERENCES tbl_skill_type(skill_type_id)
+ON DELETE NO ACTION
+ON UPDATE CASCADE;
+
+ALTER TABLE tbl_data_n
+ADD FOREIGN KEY fk_skill(skill_id)
+REFERENCES tbl_skill(skill_id)
+ON DELETE NO ACTION
+ON UPDATE CASCADE;
+
+
+
+
+
+
+
+
+
+
+-- in case we need this
 /*
 LOAD DATA LOCAL INFILE 'C:/Data/tb.csv'
 INTO TABLE tb
@@ -63,13 +93,3 @@ elderly = nullif(@elderly,-1)
 
 
 
-
-
-
-
-
-
-
-
-
--- scaledValue = (rawValue - min) / (max - min);
